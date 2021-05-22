c0dcd3ea653cd42c38831c1c7e57e95ec1a98291
mortasebastian <mr.mchaker@gmail.com>
2021-05-22 19:58:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class Graph:
+    def __init__(self):
+        self.list_of_nodes = []
+        
+
+class Node:
+    def __init__(self):
+        self.amount_of_edges = 0
+
+
+class Edge:
+    def __init__(self):
+        self.edge_from = 1
+        self.edge_to = 1
