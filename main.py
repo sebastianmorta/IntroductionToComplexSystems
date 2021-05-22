00047b2b9f72c83e73aa17112bf55128fec2099f
mortasebastian <mr.mchaker@gmail.com>
2021-05-22 20:01:56 +0200

    graph

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 class Graph:
     def __init__(self):
         self.list_of_nodes = []
-        
+
 
 class Node:
-    def __init__(self):
-        self.amount_of_edges = 0
+    def __init__(self, amount_of_edges):
+        self.amount_of_edges = amount_of_edges
 
 
 class Edge:
-    def __init__(self):
-        self.edge_from = 1
-        self.edge_to = 1
+    def __init__(self, edge_from, edge_to):
+        self.edge_from = edge_from
+        self.edge_to = edge_to
