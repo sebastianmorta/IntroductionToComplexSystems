e6893e985ede6789f673837618dadf5daf6d02c6
mortasebastian <mr.mchaker@gmail.com>
2021-05-22 22:00:46 +0200

    graph

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 
 
@@ -8,11 +9,20 @@ class Graph:
     def assignEdgesToNodes(self):
         tmp_list = list.copy(self.list_of_nodes)
         for node in self.list_of_nodes:
+            tmp_list.remove(node)
             for i in range(node.amount_of_edges):
-                node.assignEdges(self.chooseConnection())
+                node.assignEdges(random.choice(tmp_list))
 
     def chooseConnection(self):
         return randint(0, len(self.list_of_nodes))
+
+    def printer(self):
+        for node in self.list_of_nodes:
+            print("node number: ", node.number_of_node)
+            print("amount of edges: ", node.amount_of_edges)
+            for edge in node.edges:
+                print("edge from: ", edge.edge_from, "edge to: ", edge.edge_to)
+            print("===================================================================")
 
 
 class Node:
@@ -22,10 +32,15 @@ class Node:
         self.edges = []
 
     def assignEdges(self, other):
-        self.edges.append(Edge(self, other))
+        self.edges.append(Edge(self.number_of_node, other.number_of_node))
 
 
 class Edge:
     def __init__(self, edge_from, edge_to):
         self.edge_from = edge_from
         self.edge_to = edge_to
+
+
+g = Graph(10, [7, 4, 3, 2, 2, 2, 1, 1, 1, 1])
+g.assignEdgesToNodes()
+g.printer()
