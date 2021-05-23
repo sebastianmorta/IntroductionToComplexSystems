f0eaa6f67c23ccd18f6f50047fabab9ec8a6db8c
mortasebastian <mr.mchaker@gmail.com>
2021-05-23 12:38:22 +0200

    graph

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import csv
 import random
 from random import randint
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def removeDuplicates(lst):
@@ -13,6 +18,7 @@ def removeDuplicates(lst):
 class Graph:
     def __init__(self, amount_of_nodes, list_of_edges):
         self.nodes = [Node(list_of_edges[i], i) for i in range(amount_of_nodes)]
+        self.edges = []
 
     def assignEdgesToNodes(self):
         iter = 1
@@ -21,7 +27,7 @@ class Graph:
             if not tmp_list:
                 continue
             print("$$$$$$$$$$$$$$$$$$$$new$$$$$$$$$$$$$$$$$$$$$$$$")
-            for i in range(node.amount_of_edges):
+            for i in range(node.amount_of_edges - len(node.edges)):
                 print("nodes  ", len(self.nodes))
                 print("tmp  ", len(tmp_list))
                 print("iter  ", iter)
@@ -43,13 +49,13 @@ class Graph:
             print("amount of edges: ", node.amount_of_edges)
             for edge in node.edges:
                 print("edge from: ", edge.edge_from, "edge to: ", edge.edge_to)
-                h.append(edge)
+                self.edges.append(edge)
             print("===================================================================")
-        print(h)
-        print(len(h))
-        h = removeDuplicates(h)
-        print(h)
-        print(len(h))
+        print(self.edges)
+        print(len(self.edges))
+        self.edges = removeDuplicates(self.edges)
+        print(self.edges)
+        print(len(self.edges))
 
 
 class Node:
@@ -74,7 +80,7 @@ class Edge:
 
 def equality(a):
     edge_list = []
-    l = int((a / 10) ** (-a)) + 1
+    l = int((a / 10) ** (-a)) + 10
     tmp = [x * 0.1 + a / 10 for x in range(0, l)]
     for i in tmp:
         edge_list.append(int(i ** (-a)) if i ** (-a) >= 1 else 1)
@@ -83,11 +89,31 @@ def equality(a):
 
 a, b = equality(2.5)
 # g = Graph(10, [7, 4, 3, 2, 2, 2, 1, 1, 1, 1])
-g = Graph(a,b)
+g = Graph(a, b)
 g.assignEdgesToNodes()
 g.printer()
-
+print(sum(b))
 # a, b = equality(2.5)
 # print(a)
 # g=[1,2,3,4,5,6,7,8,9]
 # print(g[1:])
+# df = pd.read_csv("book1.csv")
+# print(df)
+# df = df.loc[df['weight'] > 10, :]
+# print(df)
+# df1 = df[['Source', 'Target']]
+# print(df1)
+
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "from", "to"])
+    for i in range(len(g.edges)):
+        writer.writerow([i, g.edges[i].edge_from,g.edges[i].edge_to])
+
+
+df = pd.read_csv("innovators.csv")
+df1 = df[['from', 'to']]
+G = nx.Graph()
+G = nx.from_pandas_edgelist(df1, 'from', 'to')
+nx.draw(G, with_labels=True)
+plt.show()
